fcb77a09c1af1076f244b27017214819cd08cbb5
Rebot281208 <renato-mj@rovai.eu>
2025-07-09 15:35:45 +0200

    Fix GitHub Pages deploy: add NEXT_PUBLIC_BASE_PATH to build workflow

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
   distDir: 'out',
+  basePath: '/sms-international',
+  assetPrefix: '/sms-international',
   images: {
     unoptimized: true
   }
